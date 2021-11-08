f50688e3e34c83a2a03606cf2263b0d07518a4ec
HiggsBosonic <natstonecipher@gmail.com>
2021-11-08 14:52:37 -0500

    Update lib.rs

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Df4X8UrL3MCPHrirKa88hpNYHtZXJuR9yRbSg4qggFhL");
 
 #[program]
 pub mod myepicproject {
